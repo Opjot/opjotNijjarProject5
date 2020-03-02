6f1d6703b7faab6cd0be8a7e49facc7f6d5d7910
Opjot Singh Nijjar <opjot-singh-nijjar@Opjots-MacBook-Pro.local>
2020-03-02 17:21:45 -0500

    team splitting

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ class App extends Component {
     this.state = {
       namesArray:[],
       userInput:'',
+      teamOne:[],
+      teamTwo:[],
+      random:null,
     }
   }
 
@@ -51,16 +54,35 @@ class App extends Component {
     dbRef.child(nameId).remove()
   }
 
+  randomize = () => {
+    const teamOnePlaceholder = [];
+    const teamTwoPlaceholder = [];
+  this.state.namesArray.map((name, index) => {
+    if(index % 2 === 0){
+      teamOnePlaceholder.push(name);
+    }else
+    {
+    teamTwoPlaceholder.push(name)
+    }
+  
+    this.setState({
+      teamOne: teamOnePlaceholder,
+      teamTwo: teamTwoPlaceholder,
+    })
+  })
+  };
+
+
   render(){
     return (
       <div className="App">
         <header>
           <h1>Teamify</h1>
           <div>
-              <h2>instruction</h2>
+              <h2>Instruction</h2>
               <p>1. Insert names of all players into the text box one by one.</p>
               <p>2. click randomize!</p>
-              <p>3. You will receive 2 randomly generated teams.</p>
+              <p>3. You will receive two randomly generated teams.</p>
           </div>
         </header>
         <main>
@@ -75,7 +97,7 @@ class App extends Component {
               />
               <button type="submit" onClick={this.handleClick}> Add a name </button>
             </form>
-            <button type="submit">Randomize!</button>
+            <button type="submit" onClick={this.randomize}>Randomize!</button>
           </div>
           <div>
             <div className="name">
@@ -93,11 +115,23 @@ class App extends Component {
             </div>
             <div className="team2">
               <h3>Team 1</h3>
-              {/* add p tag which pulls the names from object for team 1 and displays them using variables */}
+              {this.state.teamOne.map((nameObject) => {
+                return (
+                  <div key={nameObject.key}> 
+                    <p>{nameObject.name}</p>
+                  </div>
+                )
+                })}
             </div>
             <div className="team2">
               <h3>Team 2</h3>
-              {/* add p tag which pulls the names from object for team 2 and displays them using variables */}
+              {this.state.teamTwo.map((nameObject) => {
+                return (
+                  <div key={nameObject.key}> 
+                    <p>{nameObject.name}</p>
+                  </div>
+                )
+                })}
             </div>
           </div>
         </main>
