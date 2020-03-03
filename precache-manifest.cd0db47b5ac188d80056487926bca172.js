0257d765624b8f38881d5f4de538a14a7a451db8
Opjot Singh Nijjar <opjot-singh-nijjar@Opjots-MacBook-Pro.local>
2020-03-03 18:46:39 -0500

    Updates

---
+++ precache-manifest.cd0db47b5ac188d80056487926bca172.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32940065056cf9506de6372839ef39ae",
+    "url": "/opjotNijjarProject5/index.html"
+  },
+  {
+    "revision": "d92c62480e5574d9505c",
+    "url": "/opjotNijjarProject5/static/css/main.cda1a060.chunk.css"
+  },
+  {
+    "revision": "9b0821e6027172bb5de6",
+    "url": "/opjotNijjarProject5/static/js/2.4a6724fe.chunk.js"
+  },
+  {
+    "revision": "80f88c8a2fc44f425b1cf753399d693e",
+    "url": "/opjotNijjarProject5/static/js/2.4a6724fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d92c62480e5574d9505c",
+    "url": "/opjotNijjarProject5/static/js/main.1000528f.chunk.js"
+  },
+  {
+    "revision": "d489e47ef1095230e964",
+    "url": "/opjotNijjarProject5/static/js/runtime-main.ee2e0aa4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/opjot-nijjar-teamify/precache-manifest.26509b61ca00221142dacd1bf008f5ed.js"
+  "/opjotNijjarProject5/precache-manifest.cd0db47b5ac188d80056487926bca172.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/opjot-nijjar-teamify/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/opjotNijjarProject5/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
