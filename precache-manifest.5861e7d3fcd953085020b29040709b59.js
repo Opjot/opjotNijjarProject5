f5f29ae5cdadd8adfa1031485e4dababe8689618
Opjot Singh Nijjar <opjot-singh-nijjar@Opjots-MacBook-Pro.local>
2020-03-03 18:24:25 -0500

    Updates

---
+++ precache-manifest.5861e7d3fcd953085020b29040709b59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd0ed723e89b271fcda5c384c0152b1",
+    "url": "/teamify/index.html"
+  },
+  {
+    "revision": "e1950cf83a0e0c399552",
+    "url": "/teamify/static/css/main.cda1a060.chunk.css"
+  },
+  {
+    "revision": "40df7d349c2ea1a0d258",
+    "url": "/teamify/static/js/2.b8b3a3d1.chunk.js"
+  },
+  {
+    "revision": "80f88c8a2fc44f425b1cf753399d693e",
+    "url": "/teamify/static/js/2.b8b3a3d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1950cf83a0e0c399552",
+    "url": "/teamify/static/js/main.1813e2eb.chunk.js"
+  },
+  {
+    "revision": "1ec0115c46e220dcdfca",
+    "url": "/teamify/static/js/runtime-main.2c2b92df.js"
+  }
+]);
